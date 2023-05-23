78341a246df260806cb83080b4fc0227eb1d1ad5
ftri10 <114916525+ftri10@users.noreply.github.com>
2023-05-23 17:00:22 +0700

    Add files via upload

---
+++ librda-project/demo-librda/src/main/java/com/librda/DemoLibrdaApplication.java
@@ -0,0 +1,13 @@
+package com.librda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLibrdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLibrdaApplication.class, args);
+	}
+
+}
---
+++ librda-project/demo-librda/src/test/java/com/librda/demolibrda/DemoLibrdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librda.demolibrda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLibrdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
